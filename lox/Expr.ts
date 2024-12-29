d152b4508ce84a0b3332a68770f34fd35f283033
tsukaryu <drgon8otsuka@gmail.com>
2024-12-29 22:08:22 +0900

    feat: visitorパターンを生成

--- lox/Expr.ts
+++ lox/Expr.ts
@@ -1,32 +1,56 @@
 import { Token } from "./Token.ts";
-type Expr = string;
-export class Binary {
+abstract class Expr {
+  abstract accept<R>(visitor: Visitor<R>): R;
+}
+interface Visitor<R> {
+  visitBinaryExpr(expr: Binary): R;
+  visitGroupingExpr(expr: Grouping): R;
+  visitLiteralExpr(expr: Literal): R;
+  visitUnaryExpr(expr: Unary): R;
+}
+export class Binary extends Expr {
   left: Expr;
   operator: Token;
   right: Expr;
   constructor(left: Expr, operator: Token, right: Expr) {
+    super();
     this.left = left;
     this.operator = operator;
     this.right = right;
   }
+  accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitBinaryExpr(this);
+  }
 }
-export class Grouping {
+export class Grouping extends Expr {
   expression: Expr;
   constructor(expression: Expr) {
+    super();
     this.expression = expression;
   }
-}
-export class Literal {
-  value: object;
-  constructor(value: object) {
-    this.value = value;
+  accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitGroupingExpr(this);
   }
 }
-export class Unary {
+export class Literal extends Expr {
+  value: object;
+  constructor(value: object) {
+    super();
+    this.value = value;
+  }
+  accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitLiteralExpr(this);
+  }
+}
+export class Unary extends Expr {
   operator: Token;
   right: Expr;
   constructor(operator: Token, right: Expr) {
+    super();
     this.operator = operator;
     this.right = right;
+  }
+  accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitUnaryExpr(this);
   }
 }
--- tool/GenerateAst.ts
+++ tool/GenerateAst.ts
@@ -11,20 +11,41 @@ function main() {
 
 function defineAst(outputDir: string, baseName: string, types: string[]) {
   const path = `${outputDir}/${baseName}.ts`;
-  const importStmt = `import { Token } from "./Token.ts";`;
-  const typeStmt = `type Expr = string;`;
 
   // ファイルを空にする
   Deno.writeTextFileSync(path, "");
 
+  // Token型をimport
+  const importStmt = `import { Token } from "./Token.ts";`;
   Deno.writeTextFileSync(path, importStmt, { append: true });
-  Deno.writeTextFileSync(path, typeStmt, { append: true });
+  // Exprクラスを定義
+  const exprAbstractClass = `abstract class Expr { abstract accept<R>(visitor: Visitor<R>): R; }`;
+  Deno.writeTextFileSync(path, exprAbstractClass, { append: true });
 
+  // Visitorのinterfaceを定義
+  const visitorInterfaceStmt = `interface Visitor<R> {`;
+  Deno.writeTextFileSync(path, visitorInterfaceStmt, { append: true });
+
+  for (const type of types) {
+    const className = type.split(":")[0].trim();
+
+    Deno.writeTextFileSync(
+      path,
+      `visit${className}Expr(expr: ${className}): R;`,
+      {
+        append: true,
+      }
+    );
+  }
+
+  Deno.writeTextFileSync(path, "}", { append: true });
+
+  // classの生成
   for (const type of types) {
     const className = type.split(":")[0].trim();
     const fields = type.split(":")[1].trim();
 
-    const classNameStmt = `export class ${className}`;
+    const classNameStmt = `export class ${className} extends Expr`;
     Deno.writeTextFileSync(path, classNameStmt, { append: true });
     const leftBrace = "{";
     Deno.writeTextFileSync(path, leftBrace, { append: true });
@@ -45,6 +66,7 @@ function defineAst(outputDir: string, baseName: string, types: string[]) {
     const leftParen = "(";
     Deno.writeTextFileSync(path, leftParen, { append: true });
 
+    // constructorの引数部分
     for (const field of fields.split(",")) {
       const name = field.split(" ")[1];
       const type = field.split(" ")[0];
@@ -57,8 +79,11 @@ function defineAst(outputDir: string, baseName: string, types: string[]) {
     const rightParen = ")";
     Deno.writeTextFileSync(path, rightParen, { append: true });
 
+    // constructor実行部分
     const constructorLeftBrace = "{";
     Deno.writeTextFileSync(path, constructorLeftBrace, { append: true });
+    const constructorSuper = "super();";
+    Deno.writeTextFileSync(path, constructorSuper, { append: true });
 
     for (const field of fields.split(",")) {
       const name = field.split(" ")[1];
@@ -68,9 +93,19 @@ function defineAst(outputDir: string, baseName: string, types: string[]) {
       Deno.writeTextFileSync(path, fieldStmt, { append: true });
     }
 
+    // constructorの閉じかっこ
     const constructorRightBrace = "}";
     Deno.writeTextFileSync(path, constructorRightBrace, { append: true });
 
+    // accept関数
+    const acceptStmt = `accept<R>(visitor: Visitor<R>): R {`;
+    Deno.writeTextFileSync(path, acceptStmt, { append: true });
+    const acceptVisitor = `return visitor.visit${className}Expr(this);`;
+    Deno.writeTextFileSync(path, acceptVisitor, { append: true });
+    const acceptRightBrace = "}";
+    Deno.writeTextFileSync(path, acceptRightBrace, { append: true });
+
+    // classの閉じかっこ
     const rightBrace = "}";
     Deno.writeTextFileSync(path, rightBrace, { append: true });
   }
